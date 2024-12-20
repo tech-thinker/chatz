a388836c6e05ff7a970b2168bb3a39ab233e70c0
Asif Mohammad Mollah <admin@mrasif.in>
2024-12-20 23:58:42 +0400

    smtp support added (#7)
    [default]
    PROVIDER=smtp
    SMTP_HOST=smtp.gmail.com
    SMTP_PORT=587
    SMTP_USE_STARTTLS=true
    SMTP_USER=<yourmailid@gmail.com>
    SMTP_PASSWORD=<app-password>
    SMTP_SUBJECT=<Your subject>
    SMTP_FROM=<from-mail@gmail.com>
    SMTP_TO=<user1@gmail.com,user1@gmail.com>

--- constants/common.go
+++ constants/common.go
@@ -1,9 +1,10 @@
 package constants
 
 const (
-    PROVIDER_SLACK="slack"
-    PROVIDER_DISCORD="discord"
-    PROVIDER_TELEGRAM="telegram"
-    PROVIDER_GOOGLE="google"
-    PROVIDER_REDIS="redis"
+	PROVIDER_SLACK    = "slack"
+	PROVIDER_DISCORD  = "discord"
+	PROVIDER_TELEGRAM = "telegram"
+	PROVIDER_GOOGLE   = "google"
+	PROVIDER_REDIS    = "redis"
+	PROVIDER_SMTP     = "smtp"
 )
--- utils/common.go
+++ utils/common.go
@@ -9,78 +9,112 @@ import (
 	"github.com/tech-thinker/chatz/config"
 )
 
-
 func LoadEnv(profile string, fromEnv bool) (*config.Config, error) {
-    if fromEnv {
-        return loadEnvFromSystemEnv()
-    } else {
-        return loadEnvFromFile(profile)
-    }
+	if fromEnv {
+		return loadEnvFromSystemEnv()
+	} else {
+		return loadEnvFromFile(profile)
+	}
 }
 
 func loadEnvFromSystemEnv() (*config.Config, error) {
-    v := viper.New()
-    v.AutomaticEnv()
+	v := viper.New()
+	v.AutomaticEnv()
 
-    // Get values from system environment
-    provider := v.GetString("PROVIDER")
-    token := v.GetString("TOKEN")
-    channelId := v.GetString("CHANNEL_ID")
-    webHookURL := v.GetString("WEB_HOOK_URL")
-    chatId := v.GetString("CHAT_ID")
-    connectionURL := v.GetString("CONNECTION_URL")
+	// Get values from system environment
+	provider := v.GetString("PROVIDER")
+	token := v.GetString("TOKEN")
+	channelId := v.GetString("CHANNEL_ID")
+	webHookURL := v.GetString("WEB_HOOK_URL")
+	chatId := v.GetString("CHAT_ID")
+	connectionURL := v.GetString("CONNECTION_URL")
+	smtpHost := v.GetString("SMTP_HOST")
+	smtpPort := v.GetString("SMTP_PORT")
+	useTLS := v.GetBool("SMTP_USE_TLS")
+	useSTARTTLS := v.GetBool("SMTP_USE_STARTTLS")
+	smtpUser := v.GetString("SMTP_USER")
+	smtpPassword := v.GetString("SMTP_PASSWORD")
+	smtpSubject := v.GetString("SMTP_SUBJECT")
+	smtpFrom := v.GetString("SMTP_FROM")
+	smtpTo := v.GetString("SMTP_TO")
 
-    var env config.Config
+	var env config.Config
 
-    env.Provider = provider
-    env.WebHookURL = webHookURL
-    env.Token = token
-    env.ChannelId = channelId
-    env.ChatId = chatId
-    env.ConnectionURL = connectionURL
+	env.Provider = provider
+	env.WebHookURL = webHookURL
+	env.Token = token
+	env.ChannelId = channelId
+	env.ChatId = chatId
+	env.ConnectionURL = connectionURL
+	env.SMTPHost = smtpHost
+	env.SMTPPort = smtpPort
+	env.UseTLS = useTLS
+	env.UseSTARTTLS = useSTARTTLS
+	env.SMTPUser = smtpUser
+	env.SMTPPassword = smtpPassword
+	env.SMTPSubject = smtpSubject
+	env.SMTPFrom = smtpFrom
+	env.SMTPTo = smtpTo
 
-    return &env, nil
+	return &env, nil
 }
 
 func loadEnvFromFile(profile string) (*config.Config, error) {
-    // Get the home directory of the user
-    homeDir, err := os.UserHomeDir()
-    if err != nil {
-        fmt.Printf("Error getting home directory: %s\n", err)
-        return nil, err
-    }
+	// Get the home directory of the user
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Printf("Error getting home directory: %s\n", err)
+		return nil, err
+	}
 
-    // Set the configuration file path
-    configPath := filepath.Join(homeDir, ".chatz.ini")
+	// Set the configuration file path
+	configPath := filepath.Join(homeDir, ".chatz.ini")
 
-    // Set the file name and type
-    viper.SetConfigFile(configPath) // full path to the config file
-    viper.SetConfigType("ini")       // or "yaml", "json", etc.
+	// Set the file name and type
+	viper.SetConfigFile(configPath) // full path to the config file
+	viper.SetConfigType("ini")      // or "yaml", "json", etc.
 
-    // Read the configuration file
-    err = viper.ReadInConfig()
-    if err != nil {
-        fmt.Printf("Error reading config file: %s\n", err)
-        return nil, err
-    }
+	// Read the configuration file
+	err = viper.ReadInConfig()
+	if err != nil {
+		fmt.Printf("Error reading config file: %s\n", err)
+		return nil, err
+	}
 
-    // Get values from the INI file
-    provider := viper.GetString(fmt.Sprintf("%s.PROVIDER", profile))
-    token := viper.GetString(fmt.Sprintf("%s.TOKEN", profile))
-    channelId := viper.GetString(fmt.Sprintf("%s.CHANNEL_ID", profile))
-    webHookURL := viper.GetString(fmt.Sprintf("%s.WEB_HOOK_URL", profile))
-    chatId := viper.GetString(fmt.Sprintf("%s.CHAT_ID", profile))
-    connectionURL := viper.GetString(fmt.Sprintf("%s.CONNECTION_URL", profile))
+	// Get values from the INI file
+	provider := viper.GetString(fmt.Sprintf("%s.PROVIDER", profile))
+	token := viper.GetString(fmt.Sprintf("%s.TOKEN", profile))
+	channelId := viper.GetString(fmt.Sprintf("%s.CHANNEL_ID", profile))
+	webHookURL := viper.GetString(fmt.Sprintf("%s.WEB_HOOK_URL", profile))
+	chatId := viper.GetString(fmt.Sprintf("%s.CHAT_ID", profile))
+	connectionURL := viper.GetString(fmt.Sprintf("%s.CONNECTION_URL", profile))
 
+	smtpHost := viper.GetString(fmt.Sprintf("%s.SMTP_HOST", profile))
+	smtpPort := viper.GetString(fmt.Sprintf("%s.SMTP_PORT", profile))
+	useTLS := viper.GetBool(fmt.Sprintf("%s.SMTP_USE_TLS", profile))
+	useSTARTTLS := viper.GetBool(fmt.Sprintf("%s.SMTP_USE_STARTTLS", profile))
+	smtpUser := viper.GetString(fmt.Sprintf("%s.SMTP_USER", profile))
+	smtpPassword := viper.GetString(fmt.Sprintf("%s.SMTP_PASSWORD", profile))
+	smtpSubject := viper.GetString(fmt.Sprintf("%s.SMTP_SUBJECT", profile))
+	smtpFrom := viper.GetString(fmt.Sprintf("%s.SMTP_FROM", profile))
+	smtpTo := viper.GetString(fmt.Sprintf("%s.SMTP_TO", profile))
 
-    var env config.Config
-    env.Provider = provider
-    env.WebHookURL = webHookURL
-    env.Token = token
-    env.ChannelId = channelId
-    env.ChatId = chatId
-    env.ConnectionURL = connectionURL
+	var env config.Config
+	env.Provider = provider
+	env.WebHookURL = webHookURL
+	env.Token = token
+	env.ChannelId = channelId
+	env.ChatId = chatId
+	env.ConnectionURL = connectionURL
+	env.SMTPHost = smtpHost
+	env.SMTPPort = smtpPort
+	env.UseTLS = useTLS
+	env.UseSTARTTLS = useSTARTTLS
+	env.SMTPUser = smtpUser
+	env.SMTPPassword = smtpPassword
+	env.SMTPSubject = smtpSubject
+	env.SMTPFrom = smtpFrom
+	env.SMTPTo = smtpTo
 
-    return &env, nil
+	return &env, nil
 }
-
